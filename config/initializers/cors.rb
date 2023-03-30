f444d953e07d8d17545c829021ae1c6fd1986f71
Kevin San Gabriel <ksangabr@sfu.ca>
2023-03-30 02:59:18 -0700

    chore: update cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://10.0.0.31:3000']
+    origins ["http://fitcity.s3-website-us-west-2.amazonaws.com"]
     resource '*', 
     headers: :any, 
     methods: [:get, :post, :put], 
-    credentials: true, expose: ["Location"]
+    credentials: true
   end
 end
